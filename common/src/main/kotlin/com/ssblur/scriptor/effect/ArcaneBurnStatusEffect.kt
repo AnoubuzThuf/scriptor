7a6a56d3ca1dce93226d2e0538f02acb4725f4f1
AnoubuzThuf <alt.bpat@gmail.com>
2025-05-31 17:07:52 +0100

    Add Freeze and ArcaneBurn actions
    Freeze is a similar, but weaker DOT to Wither. ArcaneBurn is an endgame very expensive DOT with duration/power cap increased by using specific complimentary actions in the same spell

---
+++ common/src/main/kotlin/com/ssblur/scriptor/effect/ArcaneBurnStatusEffect.kt
@@ -0,0 +1,22 @@
+package com.ssblur.scriptor.effect
+
+import net.minecraft.world.effect.MobEffect
+import net.minecraft.world.effect.MobEffectCategory
+import net.minecraft.world.entity.LivingEntity
+
+open class ArcaneFlameStatusEffect: MobEffect {
+    constructor(): super(MobEffectCategory.HARMFUL, 8954814)
+
+    constructor(mobEffectCategory: MobEffectCategory, i: Int): super(mobEffectCategory, i)
+
+    override fun applyEffectTick(entity: LivingEntity, amplifier: Int): Boolean {
+        entity.hurt(entity.damageSources().magic(), 0.1F * amplifier);
+        return true
+    }
+
+    override fun shouldApplyEffectTickThisTick(i: Int, j: Int): Boolean {
+//        Tick as quickly as possible to prevent any less-damaging effects from occurring. This makes it easier to
+//        balance, as this should be the only effective DOT while active
+        return (i % 10 == 0)
+    }
+}
---
+++ common/src/main/kotlin/com/ssblur/scriptor/effect/FreezingStatusEffect.kt
@@ -0,0 +1,21 @@
+package com.ssblur.scriptor.effect
+
+import net.minecraft.world.effect.MobEffect
+import net.minecraft.world.effect.MobEffectCategory
+import net.minecraft.world.entity.LivingEntity
+
+open class FreezingEffect: MobEffect {
+    constructor(): super(MobEffectCategory.HARMFUL, 8954814)
+
+    constructor(mobEffectCategory: MobEffectCategory, i: Int): super(mobEffectCategory, i)
+
+    override fun applyEffectTick(entity: LivingEntity, amplifier: Int): Boolean {
+        entity.hurt(entity.damageSources().freeze(), 1.0F);
+        return true
+    }
+
+    override fun shouldApplyEffectTickThisTick(i: Int, j: Int): Boolean {
+        val k = 40 shr j
+        return if (k > 0) (i % k == 0) else true
+    }
+}
